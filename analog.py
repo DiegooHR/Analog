92bee595060cca3a48bfefac0029e3fe3caf4579
DiegooHR <34553961+DiegooHR@users.noreply.github.com>
2017-12-16 23:14:52 +0100

    Update analog.py

--- analog.py
+++ analog.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-Nov 24 05:45:22
 from datetime import datetime
-f = open ('auth.log', 'r')
+X = open ('auth.log', 'r')
+dic={}
 li = []
-chu={}
 for linea in f.readlines():
-    if 'authentication failure' in linea:
-	    li.append(datetime.strptime(linea[:15],'%b %d %H:%M:%S')) 
-f.close()
-for dat in li:
-	print datetime.strftime(dat, '%m %d %H:%M:%S')
+	if 'authentication failure' in linea:
+		Y= lista[lista.find('rhost')+4:]
+		if Y.split()[0] in linea:
+			li=[]
+			tiempo=datetime.strptime(linea[:12], '%b %d %H:%M:%S')
+			li.append(linea[:12])
+			if Y.split[0] not in dic:
+				dic[Y.split[0]]=li
+
+log.close()
+print dic
